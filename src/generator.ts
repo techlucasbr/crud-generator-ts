2f92f107c1a7c03c67aff24529a890f138039f1e
Lucas G. Pereira <lucas.pereira@mouts.info>
2026-04-15 14:42:35 -0300

    feat: Melhoria na busca de query

--- src/generator.ts
+++ src/generator.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const generateFiles = (resourceName: string) => {
+const generateFilesTeste = (resourceName: string) => {
   const className = resourceName.charAt(0).toUpperCase() + resourceName.slice(1);
   const fileName = resourceName
     .replace(/([A-Z])/g, '-$1')
